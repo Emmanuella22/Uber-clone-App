3b7792de7c04a26d734b562cdeee1908e2b577ff
Emmanuella Onedibe <emmanuellaonedibe@gmail.com>
2022-04-02 22:27:34 +0100

    uber clone app

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react"
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+
+        <div className="navbar_container">
+          <div className="menu">
+             <ul>
+                 <li className="nav-items">
+                  <li>Uber</li>
+                  <li>Company</li>
+                  <li>Safety</li>
+                  <li>Help</li>
+                  <li>COVID-19 resources</li>
+                </li> 
+             </ul>
+
+         </div>
+
+     </div>
+    );
+}
+
+
+export default Navbar;
