b5798e08466afa50400aebe9962d614be9710cfd
Emmanuella Onedibe <emmanuellaonedibe@gmail.com>
2022-04-11 11:21:46 +0100

    uber-clonee

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,28 +4,26 @@ import "./Navbar.css"
 const Navbar = () => {
     return (
 
-        <div className="navbar_right">
-             <ul>
-                 <li className="nav-items">
+        <div className="navigation">
+          <div className="navbar_right">
+             <ul className="nav-items">
                   <li>Uber</li>
                   <li>Company</li>
                   <li>Safety</li>
                   <li>Help</li>
                   <li>COVID-19 resources</li>
-                </li> 
             </ul>
+          </div>
      
           <div className="navbar_left"> 
-               <ul>
-                  <li className="nav-items2">
+               <ul className="nav-items2">
                    <li>EN</li>
                    <li>Products</li>
                    <li>Log in</li>
                    <li>Sign in</li>
-                </li>
               </ul>
+          </div>
         </div>
-      </div>
     );
 }
 
